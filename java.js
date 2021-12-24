0f0d7f80d8229e310605e662db1b5dbee3a90cac
maya-abdoh <92648274+maya-abdoh@users.noreply.github.com>
2021-12-24 18:16:06 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,22 @@
-
+let btn12 =document.getElementsByClassName('btn1');
+let btn2 =document.getElementById('mm');
+let image1=document.getElementById('coverImg');
+let image12=document.getElementById('ss');
+let image123=document.getElementById('cc');
+let text=document.getElementById('text');
+let te=document.getElementsByClassName('tex');
+btn12.addEventListener('click', () =>  {
+image1.src='PIC2.png';
+} ) 
+btn12.addEventListener('click', () =>  {
+    image12.src='PIC1.png';
+} ) 
+btn12.addEventListener('click', () =>  {
+    image123.src='PIC3.png';
+} ) 
+btn12.addEventListener('click', () =>  {
+  text.textContent="maya abdohhhh";
+} ) 
+  btn12.addEventListener('click', () =>  {
+    te.textContent="maya kkkkkabdohhhh";
+  } ) 
